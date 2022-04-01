f67f7b5370afa828f914af4ce6e705fbb9e34d67
Gollapudi Sasank <102504774+Sasank-2004@users.noreply.github.com>
2022-04-01 22:49:18 +0530

    Add files via upload

---
+++ python code/code.py
@@ -0,0 +1,21 @@
+import numpy as np
+from math import sqrt
+from matplotlib import pyplot as plt
+
+
+def f(x):
+     return (2*x+sqrt(4*x**2-1))/(2*x-sqrt(4*x**2-1))
+
+
+x = np.linspace(0.5,2,num=100)
+y = [f(i) for i in x]
+y1 = [4 for i in range(100)]
+plt.xlabel("X axis ")
+plt.ylabel("Y axis")
+plt.title("My first Graph")
+plt.grid()
+plt.plot(x,y,label = 'y=f(x)')
+plt.plot(x,y1,label = 'y=4')
+plt.annotate("(0.625,4)",(0.625,4),(0.7,15),arrowprops={"arrowstyle":"<-"})
+plt.show()
+
