8a950012a1589d4b1ab572bba7f18771ba5babef
Gollapudi Sasank <102504774+Sasank-2004@users.noreply.github.com>
2022-04-03 16:26:18 +0530

    Add files via upload

---
+++ codes/code.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+float f(float x)
+{
+    return (2*x+sqrt(4*x*x-1))/(2*x-sqrt(4*x*x-1));
+}
+int main()
+{
+    float x =5.0/8;
+    if(f(x)==4) printf("Correct Answer\n");
+    else printf("Wrong Anser\n");
+    return 0;
+}
