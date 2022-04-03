d0ea85c755681ad98038b69b6071d4a132b69056
Gollapudi Sasank <102504774+Sasank-2004@users.noreply.github.com>
2022-04-03 16:38:57 +0530

    Update code.c

--- codes/code.c
+++ codes/code.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<math.h>
-float f(float x)
-{
-    return (2*x+sqrt(4*x*x-1))/(2*x-sqrt(4*x*x-1));
-}
+float f(float x);
 int main()
 {
     float x =5.0/8;
     if(f(x)==4) printf("Correct Answer\n");
     else printf("Wrong Anser\n");
     return 0;
+}
+float f(float x)
+{
+    return (2*x+sqrt(4*x*x-1))/(2*x-sqrt(4*x*x-1));
 }
